3da4f9a3912dbc7f21c3c114ecda93b4b05b4d68
Angel-grace <angelgrace2918@gmail.com>
2026-02-16 18:51:37 +0530

    2nd commit

---
+++ tests/data/testData.js
@@ -0,0 +1,10 @@
+export const testData = {
+  userInput1: "Hi", //first user input to chatbot
+  userInput2: "Tell me about your services" //query to fetch services page
+};
+
+export const urls = {
+  careplus: "https://tenant-one.pentesitesai.com/",
+  chery: "https://tenant-two.pentesitesai.com/",
+  yopmail: "https://yopmail.com/en/"
+};
